8232a8ed42878d5fa77961a420390ab97d9fbccf
Danilo Salvio <danilo_salvio@live.com>
2018-07-14 22:07:38 -0300

    Adds url to queue environment variable.

--- handler.js
+++ handler.js
@@ -1,16 +1,16 @@
 'use strict';
 
+const QUEUE_URL = process.env.QUEUE_URL;
+
 module.exports.pushToQueue = (event, context, callback) => {
+  const body = (event.body === undefined ? 'No-message' : event.body);
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: QUEUE_URL
     }),
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  
 };

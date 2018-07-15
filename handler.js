01161e2bf496fc62da9ea084fa4f56d1960014a9
Danilo Salvio <danilo_salvio@live.com>
2018-07-14 23:17:04 -0300

    Codes lambda to send message to queue.

--- handler.js
+++ handler.js
@@ -4,10 +4,25 @@ const QUEUE_URL = process.env.QUEUE_URL;
 
 module.exports.pushToQueue = (event, context, callback) => {
   const body = (event.body === undefined ? 'No-message' : event.body);
+
+  const AWS = require("aws-sdk");
+  const sqs = new AWS.SQS();
+
+  const params = {
+    MessageBody: JSON.stringify(body),
+    QueueUrl: QUEUE_URL
+  };
+
+  try {
+    sqs.sendMessage(params).promise();
+  } catch(error) {
+    console.log(error);
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: QUEUE_URL
+      message: body
     }),
   };
 
